a78ae94e45f6d086ec3288253495c1f7f9c31eb6
Lawrence Stanton <52958935+LawrenceStanton@users.noreply.github.com>
2024-06-06 01:59:04 +0200

    New analysis approach. New main notebook. Helper functions in respective .py files.

---
+++ Src/eisImport.py
@@ -0,0 +1,57 @@
+import os as os
+import pandas as pd
+
+from typing import List, Dict
+from dataclasses import dataclass
+
+def findEisFiles(eisDirectory:str, isAll:bool=False) -> List[str]:
+	files = []
+	for root, dirs, file in os.walk(eisDirectory):
+		for f in file:
+			if isAll:
+				if f.endswith('_EIS00001.csv') and root.endswith('All'):
+					files.append(os.path.join(root, f))
+			else:
+				if f.endswith('_EIS00001.csv') and not root.endswith('All'):
+					files.append(os.path.join(root, f))
+	
+	return files
+
+def findMainEisFiles(eisDirectory:str) -> List[str]:
+	return findEisFiles(eisDirectory, isAll=False)
+
+def findAllEisFiles(eisDirectory:str) -> List[str]:
+	return findEisFiles(eisDirectory, isAll=True)
+
+@dataclass
+class EisData:
+	data: pd.DataFrame
+	metadata: Dict[str, str]
+
+def readEisFile(file:str) -> EisData:
+	headerRow = 30
+	skipRow = lambda r: (r in range(0, headerRow-1)) or (r in range(headerRow, headerRow + 4)) # Constant offset for all EIS files.
+
+	data = pd.read_csv(
+		file,
+		skiprows = skipRow,
+		usecols = lambda col: col not in ["Voltage", "Current", "Cycle", "Cycle Level", "EisStart", "EisFinish", "AAcMax", "Unnamed: 42"],
+	)
+
+	metadata = pd.read_csv(
+		file,
+		nrows=23,
+		skiprows=2,
+		sep=',',
+		names=['key', 'value'],
+	)
+	metadataDict:Dict = metadata.set_index('key').to_dict()['value']
+
+	return EisData(data, metadataDict)
+
+def readEisFiles(files: List[str]) -> Dict[str, EisData]:
+	eis = {}
+	for file in files:
+		eisData = readEisFile(file)
+		eis[eisData.metadata['Comment']] = eisData	# Comment set as unique key during experiment.
+	return eis
---
+++ Src/eisPlot.py
@@ -0,0 +1,127 @@
+import os
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+
+from eisImport import EisData
+from typing import Dict, Tuple
+
+
+def plotNyquist(
+    eis: Dict[str, EisData],
+    realLabel="Real Impedance (mΩ)",
+    imagLabel="Imaginary Impedance (mΩ)",
+    ax: Axes | None = None,
+    scatter: bool = False,
+    saveDir: str | None = None,
+    transparent: bool = False,
+    title: str | None = None,
+    figsize: Tuple[int, int] = (16, 12),
+) -> Axes | None:
+    """
+    Plots the EIS spectral data with a Nyquist scatter plot.
+
+    Args:
+        eis (Dict[str, EisData]): A dictionary containing EIS spectral data.
+        realLabel (str, optional): Label for the real impedance axis. Defaults to "Real Impedance (mΩ)".
+        imagLabel (str, optional): Label for the imaginary impedance axis. Defaults to "Imaginary Impedance (mΩ)".
+        ax (Axes | None, optional): The matplotlib Axes object to plot on. Defaults to None.
+        scatter (bool, optional): If the plot should be a scatter plot. Defaults to False (line plot).
+        saveDir (str | None, optional): The directory to save the plot image. Defaults to None.
+        transparent (bool, optional): If the saved plot should be transparent. Defaults to False. Only effects if saveDir is not None.
+        title (str | None, optional): The title of the plot. Defaults to None.
+        figsize (Tuple[int, int], optional): The size of the plot figure. Defaults to (16, 12).
+
+    Single EIS special effects (`len(eis) == 1`):
+    - Title is the spectrum key name.
+    - Subtitle with metadata information.
+    - Removes the legend.
+
+    Multiple EIS data special effects:
+    - Title is the provided title (if provided).
+    - Legend is shown.
+
+    Returns:
+        Axes | None: The matplotlib Axes object if saveDir is None, otherwise None.
+
+    Raises:
+        ValueError: If no EIS data is provided.
+
+    Industry standard Nyquist plot settings:
+    - Equal aspect ratio.
+    - Inverts the imaginary axis.
+    - Zero left limit on the real axis.
+
+    Example usage:
+    ```
+    eis_data = {
+        "Spectrum1": eis_data1,
+        "Spectrum2": eis_data2,
+    }
+    plot = plotNyquist(eis_data, realLabel="Real Impedance (mΩ)", imagLabel="Imaginary Impedance (mΩ)")
+    ```
+    """
+
+    if len(eis) == 0:
+        raise ValueError("No EIS data provided.")
+
+    for spectra in eis:
+        plot_func = (
+            eis[spectra].data.plot.scatter if scatter else eis[spectra].data.plot.line
+        )
+        plot: Axes = plot_func(
+            x="Zreal1",
+            y="Zimg1",
+            marker=".",
+            ax=ax,
+            label=spectra,
+        )
+        ax = plot
+
+    plot.invert_yaxis()
+    plot.set_aspect("equal", adjustable="box")
+    plot.grid(axis="both")
+    plot.set_xlim(left=0)
+    plot.set_xlabel(realLabel)
+    plot.set_ylabel(imagLabel)
+
+    figure = plot.get_figure()
+    assert figure is not None
+
+    figure.set_size_inches(figsize)
+
+    if len(eis) == 1:
+        title = list(eis.keys())[0]
+        assert title is not None
+        plot.set_title(title)
+
+        suptitle = f"""
+        Measurement ID: {eis[title].metadata['Measurement ID']}
+        Start Time: {eis[title].metadata['Start Time']}
+        End Time: {eis[title].metadata['End Time']}
+        """
+        figure.suptitle(
+            suptitle,
+            ha="left",
+            x=0.125,
+        )
+
+        plot.legend().remove()
+    else:
+        plot.legend()
+        if title is not None:
+            plot.set_title(title)
+
+    if saveDir is not None:
+        os.makedirs(saveDir, exist_ok=True)
+        savePath = os.path.join(saveDir, f"{spectra}.png")
+
+        assert plot.figure is not None
+        figure.savefig(
+            savePath,
+            transparent=transparent,
+        )
+        plt.close(figure)  # Memory management
+
+        return None
+    else:
+        return plot
